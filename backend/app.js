c19efef8cd223f7ef86664f9a4e8f2bfbf418cd6
aniketjamkar007 <aniketjamkar@gmail.com>
2026-03-29 17:05:52 +0530

    adding dummy json and updating post model

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: 'post-1',
+            title: 'First server side post',
+            content: 'This is coming from the server'
+        },
+        {
+            id: 'post-2',
+            title: 'second server side post',
+            content: 'This is coming from the server'
+        },
+        {
+            id: 'post-3',
+            title: 'third server side post',
+            content: 'This is coming from the server'
+        },
+    ]
+    res.status(200).json({
+        message: 'posts fetched successfully',
+        posts: posts
+    })
 });
 
 module.exports = app;
