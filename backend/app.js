a9caa6e2fb74f0b291c50ffd82b8614513543d0a
aniketjamkar007 <aniketjamkar@gmail.com>
2026-03-29 18:30:16 +0530

    adding get and post http calls to angular

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,33 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+    );
+    next();
+});
+
+app.post('/api/post', (req,res,next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'posts added successfully'
+    })
+    next();
+})
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         {
             id: 'post-1',
@@ -19,7 +44,7 @@ app.use('/api/posts', (req, res, next) => {
             title: 'third server side post',
             content: 'This is coming from the server'
         },
-    ]
+    ];
     res.status(200).json({
         message: 'posts fetched successfully',
         posts: posts
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,15 @@ export class PostsService {
   private posts:Post[] =[];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    return [...this.posts];
+    return this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((res) =>{
+      this.posts=res.posts;
+      this.postsUpdated.next([...this.posts]);
+    })
+    // return [...this.posts];
   }
 
   getPostUpdateListener() {
@@ -21,10 +27,15 @@ export class PostsService {
 
   addPost(newPost:Post) {
     const post:Post = {
+      id: null,
       title: newPost.title,
       content: newPost.content
     }
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts])
+    this.http.post<{message: string}>('http://localhost:3000/api/post', post)
+      .subscribe((resData) => {
+        console.log(resData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      })
   }
 }
